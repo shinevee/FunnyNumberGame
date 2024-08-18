95abfd991608051f177ffe86620de3efc7cc841b
Random Person <149548280+shinevee@users.noreply.github.com>
2024-08-18 11:53:03 -0400

    customizable difficulty part 2
    why is it considered local if I don't assign it something before using it in 2 functions but not if I do?

--- Program.cs
+++ Program.cs
@@ -1,21 +1,36 @@
 ﻿// initalization
 byte minrange = 1;
-byte maxrange = 10;
+byte maxrange;
 byte fails = 0;
 int points = 0;
 bool correct = true;
 byte number = 0;
 byte guess;
-byte easyMode;
+byte easyMode = 0;
 byte maxFails;
 
-// startup
 Console.WriteLine("Welcome to the funny number game!");
 Console.WriteLine("(Shinevee Edition)");
 
-Console.WriteLine("");
 
-Console.WriteLine("How many tries would you like to be able to make before Game Over?");
+// setup
+Console.WriteLine("\nWhat should the highest possible number generated be?");
+Console.WriteLine("*Must be a number between \"2\" and \"255\"");
+Console.WriteLine("**If the answer is not a number between 2 and 255, it shall be inputted as \"10\"");
+try
+{
+    maxrange = byte.Parse(Console.ReadLine());
+}
+catch (Exception)
+{
+    maxrange = 10;
+}
+if (maxrange < 2)
+{
+    maxrange = 10;
+}
+
+Console.WriteLine("\nHow many tries would you like to be able to make before Game Over?");
 Console.WriteLine("*Must be a number between \"1\" and \"255\"");
 Console.WriteLine("**If the answer is not a number between 1 and 255, it shall be inputted as \"4\"");
 try
@@ -30,12 +45,14 @@ if (maxFails == 0) {
     maxFails = 4;
 }
 
-Console.WriteLine("");
+if (maxFails > 1)
+{
+    Console.WriteLine("\nType \"1\" if you would like to regain a lost try for each correct guess");
+    try { easyMode = byte.Parse(Console.ReadLine()); } catch (Exception) { easyMode = 0; }
+    Console.WriteLine("");
+}
 
-Console.WriteLine("Type \"1\" if you would like to regain a lost try for each correct guess");
-try { easyMode = byte.Parse(Console.ReadLine()); } catch (Exception) { easyMode = 0; }
-
-
+// game logic
 while (fails < maxFails)
 {
     if (correct == true)
@@ -62,20 +79,20 @@ while (fails < maxFails)
     {
         if (guess > number)
         {
-            Console.WriteLine("It's lower than " + guess);
+            Console.WriteLine("\nIt's lower than " + guess);
         }
         else
         {
-            Console.WriteLine("It's higher than " + guess);
+            Console.WriteLine("\nIt's higher than " + guess);
         }
         fails++;
         if (fails < (maxFails - 1))
         {
-            Console.WriteLine(maxFails - fails + " mistakes until Game Over");
+            Console.WriteLine(maxFails - fails + " mistakes until Game Over\n");
         }
         else if (fails == (maxFails - 1))
         {
-            Console.WriteLine(maxFails - fails + " mistake until Game Over");
+            Console.WriteLine(maxFails - fails + " mistake until Game Over\n");
         }
         guess = 0;
     }
@@ -85,17 +102,24 @@ while (fails < maxFails)
         points++;
         if (easyMode == 1 && fails > 0)
             {       
-                   Console.WriteLine("+1 possible mistakes");
+                   Console.WriteLine("+1 possible mistakes\n");
                    fails--;
             }
         guess = 0;
         correct = true;
+        Console.WriteLine("");
     }
 }
 while (fails == maxFails)
 {
-    Console.WriteLine("Game Over");
-    Console.WriteLine("You guessed " + points + " numbers correctly");
+    Console.WriteLine("\nGame Over!");
+    if (points != 1)
+    {
+        Console.WriteLine("You guessed " + points + " numbers correctly");
+    }
+    else {
+        Console.WriteLine("You guessed " + points + " number correctly");
+    }
     string here = (Console.ReadLine());
     fails++;
 }
